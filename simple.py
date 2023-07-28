172be466cd0b2f75f4ff0fd74c0693397a311f79
Josh <brink66@gmail.com>
2023-07-28 15:00:15 +0100

    First commit

---
+++ simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+  simple.py	-	Simple Python procedure
+
+  AUTHOR
+    Written by John Brink
+
+  REVISION HISTORY
+  -------------------------------------------------------
+    1.0 07/28/2021 jbr - Display 'Hello World'
+  -------------------------------------------------------
+'''
+
+print('Hello World')
