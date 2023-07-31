b76d985acd053a81d5ac9693c1a869c12f16eaac
John Brink <brink66@gmail.com>
2023-07-31 08:57:10 +0200

    Play with output

--- simple.py
+++ simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
   NAME
     simple.py	-	Simple Python procedure
@@ -9,7 +9,22 @@
   REVISION HISTORY
   -------------------------------------------------------
     1.0 07/28/2021 jbr - Display 'Hello World'
+    1.1	07/30/2021 j6b - Play with output
   -------------------------------------------------------
 '''
 
+import os
+import sys
+import argparse
+
+t       = ('Hello', 'World', 'Britt & Joris', 'Lonneke', 'Nicole Le Haen')
+ln      = 1
+header  = ['Nr', 'Omschrijving', 'Balans']
+
 print('Hello World')
+print("%-2s %-20s %6s"%(header[0],header[1],header[2]))
+print(40*'-')
+
+for i in t * 3:
+	print("%2d %-20s %6s"%(ln,i,(ln%2 == 0)))
+	ln += 1
