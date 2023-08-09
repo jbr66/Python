56d3207b7db6833727e4487be0638e73c9820daa
John Brink <brink66@gmail.com>
2023-08-09 12:16:22 +0200

    add parameters to iprange.py

--- iprange.py
+++ iprange.py
@@ -13,12 +13,39 @@
 '''
 
 from ipaddress import ip_address
+import argparse
 
+# Define functions
 def ips(start, end):
     '''Return IPs in IPv4 range, inclusive.'''
     start_int = int(ip_address(start).packed.hex(), 16)
     end_int = int(ip_address(end).packed.hex(), 16)
     return [ip_address(ip).exploded for ip in range(start_int, end_int)]
 
+# Define parameters
 
-print(ips('192.168.1.240', '192.168.2.5'))
+parser = argparse.ArgumentParser(
+               description="generate list of IP-addresses")
+parser.add_argument('start',
+               help="provide start address")
+parser.add_argument('end',
+               help="provide end address")
+parser.add_argument('-d', '--debug',
+               help="display debug information",
+               action="store_true")
+parser.add_argument('-v', '--verbosity', action="count", default=0,
+               help="increase output verbosity")
+args = parser.parse_args()
+
+# Setting variables
+start   = args.start
+end     = args.end
+debug   = args.debug
+verbose = args.verbosity
+
+if debug:
+  print(42*'-')
+  print(args)
+  print(42*'-')
+
+print(ips(start, end))
