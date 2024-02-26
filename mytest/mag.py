bf404d8962c440a67ada2989f63b661c4e49cfca
Josh <brink66@gmail.com>
2024-02-26 11:34:24 +0000

    Add students

--- mytest/mag.py
+++ mytest/mag.py
@@ -40,13 +40,20 @@ except Exception as e:
 	print("Error reading %s - %s" % (file,e))
 	sys.exit(1)
 
-line = '+' + 29*'-' + '+'
-print(line)
-header = '| %11s | %4s | %5s |' % ('vak'.center(11),'grade','round')
-print(header)
-print(line) 
-for v in data.keys():
-	mean = calculateMean(data[v])
-	print('| %11s | %5.2f | %3.0f   |' % (v,mean,round(mean,0)))
-
-print(line)
+for s in data.keys():
+	print('Student %s:' % s.upper())
+	line = '+' + 29*'-' + '+'
+	print(line)
+	header = '| %11s | %4s | %5s |' % ('vak'.center(11),'grade','round')
+	print(header)
+	print(line) 
+	somMean = 0
+	for v in data[s].keys():
+		mean = calculateMean(data[s][v])
+		somMean += mean
+		print('| %11s | %5.2f | %3.0f   |' % (v,mean,round(mean,0)))
+	
+	print(line)
+	print('| %11s | %5.2f | %3.0f   |' % ('Mean'.center(11),somMean/len(data[s].keys()),round(somMean/len(data[s].keys()),0)))
+	print(line)
+	print()
