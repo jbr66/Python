c6c1773dc80ad09e8c5ae0f88c16a9d44852abce
jbr66 <brink66@gmail.com>
2024-03-13 12:33:12 +0100

    Use JSON output

--- QAD/getschema.py
+++ QAD/getschema.py
@@ -14,8 +14,70 @@ Using config.yaml file:
 
 import mariadb
 import yaml
+import json
 import argparse
 import os, sys
+
+def con_mariadb(cfg):
+    '''
+    Connect to MariaDb from configfile, will return the connection
+    '''
+    try:
+        conn = mariadb.connect(
+            user     = cfg['user'],
+            password = cfg['password'],
+            host     = cfg['host'],
+            port     = cfg['port'],
+            database = cfg['database']
+        )
+        return conn
+    except mariadb.Error as e:
+        print('Error connecting to MariaDB Platform %s' % e)
+        sys.exit(1)
+
+def get_tables(conn):
+    '''
+    Returns dictionary of tables
+    '''
+
+    tables = {}
+    # Get cursor
+    _cur  = conn.cursor()
+
+    _cur.execute('SHOW tables')
+
+    for t in _cur:
+        tables[t[0]] = {}
+
+    return tables
+
+def get_fields(conn, table):
+    '''
+    Returns dictionary of fields for given table
+    '''
+
+    fields = {}
+    # Get cursor
+    _cur = conn.cursor()
+
+    qry = 'describe %s' % table
+    _cur.execute(qry)
+    for f, t, n, k, d, e in _cur:
+        fields[f] = {}
+        if '(' in t:
+            type = t.strip().split('(')[0]
+            format = t.strip().split('(')[1][:-1]
+        else:
+            type = t
+            format = ''
+        fields[f]['type'] = type
+        fields[f]['format'] = format
+        fields[f]['null'] = n
+        fields[f]['key'] = k
+        fields[f]['default'] = d
+        fields[f]['extra'] = e
+
+    return fields
 
 # Define parameters
 
@@ -28,6 +90,7 @@ parser.add_argument('file',
 
 args        = parser.parse_args()
 config_file = args.file
+schema      = {}
 
 if not os.path.isfile(config_file):
     print('File %s could not be found - Exiting' % config_file)
@@ -44,61 +107,25 @@ except Exception as e:
     sys.exit(3)
 
 # Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user     = cfg['user'],
-        password = cfg['password'],
-        host     = cfg['host'],
-        port     = cfg['port'],
-        database = cfg['database']
-    )
-except mariadb.Error as e:
-    print('Error connecting to MariaDB Platform %s' % e)
-    sys.exit(1)
+conn = con_mariadb(cfg)
 
-# Define variables
-schema = {}
-tables = {}
+# Built schema
+schema[cfg['database']] = get_tables(conn)
 
-# Get cursor
-cur  = conn.cursor()
-
-cur.execute('SHOW tables')
-
-for t in cur:
-    tables[t[0]] = {}
-
-schema[cfg['database']] = tables
-print('\nTables in %s' % cfg['database'])
-print(20*'-')
 for table in schema[cfg['database']].keys():
-    fields = {}
-    print('\t%s' % table)
-    qry = 'describe %s' % table
-    cur.execute(qry)
-    for f, t, n, k, d, e in cur:
-        fields[f] = {}
-        if '(' in t:
-            type = t.strip().split('(')[0]
-            format = t.strip().split('(')[1][:-1]
-        else:
-            type = t
-            format = ''
-        fields[f]['type'] = type
-        fields[f]['format'] = format
-        fields[f]['null'] = n
-        fields[f]['key'] = k
-        fields[f]['default'] = d
-        fields[f]['extra'] = e
-    schema[cfg['database']][table] = fields
+    schema[cfg['database']][table] = get_fields(conn,table)
 
-print("\nFields for table countries:")
-print(len('Fields for table countries:')*'-')
-for f in schema[cfg['database']]['countries'].keys():
-    print('Field: %s' % f)
-    print(len('Field: %s' % f)*'-')
-    for k in schema[cfg['database']]['countries'][f].keys():
-        print('%s : %s' % (k,schema[cfg['database']]['countries'][f][k]))
-    print()
+print(json.dumps(schema, indent=3))
+'''
+for t in schema[cfg['database']].keys():
+    print('\nFields for table %s:' % t)
+    print(len('Fields for table %s:' % t)*'-')
+    for f in schema[cfg['database']][t].keys():
+        print('Field: %s' % f)
+        print(len('Field: %s' % f)*'-')
+        for k in schema[cfg['database']][t][f].keys():
+            print('%s : %s' % (k,schema[cfg['database']][t][f][k]))
+        print()
+'''
 
 conn.close()
