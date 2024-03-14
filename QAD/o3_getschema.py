45f64e4b141aaef7bcc7fbca4976f7e2224354ea
John Brink <brink66@gmail.com>
2024-03-14 11:04:06 +0100

    Add named changed tables to compare

--- QAD/o3_getschema.py
+++ QAD/o3_getschema.py
@@ -19,14 +19,30 @@ import argparse
 import os, sys
 import glob
 
+'''
+    Questions:
+        blob -> mediumtext (longblob)
+        datetime-tz -> datetime + offset field (timestamp/int)
+        raw -> mediumtext (varbinary)
+'''
+
 map_dt_oem_mdb = [
         ('character', 'varchar'),
         ('character', 'text'),
+        ('blob', 'mediumtext'),
+        ('clob', 'mediumtext'),
         ('logical', 'tinyint'),
         ('integer', 'int'),
         ('int64', 'bigint'),
         ('date', 'date'),
+        ('datetime', 'datetime'),
+        ('datetime-tz', 'datetime'),
         ('decimal', 'decimal'),
+        ('raw', 'mediumtext'),
+        ('blob', 'longblob'),
+        ('datetime-tz', 'timestamp'),
+        ('datetime-tz', 'int'),
+        ('raw', 'varbinary'),
         ]
 
 def con_mariadb(cfg, mdb):
@@ -115,18 +131,24 @@ def oem_getfields(file):
 
     return fields
 
-def compare_table(mdb, table):
+def compare_table(mdb, table, changename):
     '''
     Compare the table in both OpenEdge Database and MariaDB
     It will return a dictionary for each table
     '''
     compare = {}
+    if table in changename:
+        oetable = table
+        mdbtable = table + '_'
+    else:
+        oetable = table
+        mdbtable = table
 
-    for f in oeschema[table].keys():
-        if f in schema[mdb][table]:
+    for f in oeschema[oetable].keys():
+        if f in schema[mdb][mdbtable]:
             compare[f] = {}
-            compare[f]['type'] = (oeschema[table][f]['type'],schema[mdb][table][f]['type'])
-            compare[f]['format'] = (oeschema[table][f]['format'],schema[mdb][table][f]['format'])
+            compare[f]['type'] = (oeschema[oetable][f]['type'],schema[mdb][mdbtable][f]['type'])
+            compare[f]['format'] = (oeschema[oetable][f]['format'],schema[mdb][mdbtable][f]['format'])
 
     return compare
 
@@ -188,7 +210,7 @@ for t in schema[cfg['database']].keys():
 
 # Verify tables from OpenEdge with MariaDB
 #dir = ('../stage_qadeam', '../stage_qadadm', '../stage_qaddb')
-dir = ('../stage_qadeam',)
+dir = ('../oeschema',)
 
 schema_suffix = 'dat'
 index_suffix = 'idx'
@@ -205,7 +227,6 @@ for d in dir:
         oetables.append(oefile.split('.')[0])
         oeschema[oefile.split('.')[0]] = fields
 print('Number of tables (OpenEdge databases): %d' % len(oeschema.keys()))
-print()
 
 notfound = []
 for oetable in oetables:
@@ -216,28 +237,37 @@ for oetable in oetables:
     if found == False:
         notfound.append(oetable)
 
-print('Not found in MariaDB: %s' % notfound)
+changename = []
 for oetable in notfound:
-    oetable = oetable + '_'
+    t = oetable + '_'
     for db in ('o3_qaddb', 'o3_sysdb'):
-        if oetable in schema[db]:
-            print('Found %s in %s' % (oetable, db))
+        if t in schema[db].keys():
+            changename.append(oetable)
+            break
+
+# Remove tables with changed names from notfound[]
+for t in changename:
+    if t in notfound:
+        notfound.remove(t)
+
+print('Tables with changed names - %s' % changename)
+print('Not found in MariaDB: %s' % notfound)
 
 print()
 # Verify OpenEdge tables with tables in MariaDB
 compare = {}
 for oetable in oeschema.keys():
+    if oetable in notfound:
+        continue
     if oetable in schema['o3_qaddb']:
-        print('Table in o3_qaddb')
         mdbname = 'o3_qaddb'
     elif oetable in schema['o3_sysdb']:
-        print('Table in o3_sysdb')
         mdbname = 'o3_sysdb'
 
-    compare[oetable] = compare_table(mdbname, oetable)
+    compare[oetable] = compare_table(mdbname, oetable, changename)
 
     for field in compare[oetable].keys():
         if not compare[oetable][field]['type'] in map_dt_oem_mdb:
-            print('Field %s of %s datatype issue' % (field, oetable))
+            print('Field %s of %s.%s datatype issue - %s' % (field, mdbname, oetable, compare[oetable][field]['type']))
 
-print(json.dumps(compare, indent=3))
+#print(json.dumps(compare['Property'], indent=3))
