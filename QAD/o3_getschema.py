0b10aaa282514ea08fde5704ed57a7034f4ded1d
John Brink <brink66@gmail.com>
2024-03-14 16:21:56 +0100

    Add logging

--- QAD/o3_getschema.py
+++ QAD/o3_getschema.py
@@ -18,6 +18,7 @@ import json
 import argparse
 import os, sys
 import glob
+import logging
 
 '''
     Questions:
@@ -54,6 +55,14 @@ dt_no_fmt = [
         'date',
         'mediumtext',
         'longblob',
+        ]
+
+valid_loglevel = [
+        'DEBUG',
+        'INFO',
+        'WARNING',
+        'ERROR',
+        'CRITICAL',
         ]
 
 def con_mariadb(cfg, mdb):
@@ -172,140 +181,185 @@ parser = argparse.ArgumentParser(
 parser.add_argument('file',
         help="provide yaml based configfile"
         )
+parser.add_argument('--loglevel', dest='loglevel', default='INFO',
+        help='Provide loglevel (default is INFO)'
+        )
+parser.add_argument('--logfile', dest='logfile', default='getschema.log',
+        help='Provide logfile (default is getschema.log)'
+        )
 
 args        = parser.parse_args()
 config_file = args.file
+loglevel    = args.loglevel.upper()
+logfile     = args.logfile
 
-if not os.path.isfile(config_file):
-    print('File %s could not be found - Exiting' % config_file)
-    sys.exit(1)
+if __name__ == '__main__':
 
-try:
-    with open(config_file, 'r') as file:
-        cfg = yaml.safe_load(file)
-except PermissionError:
-    print('Not enough permissions to open file %s' % config_file)
-    sys.exit(2)
-except Exception as e:
-    print('Failed to open file %s - %s' % (config_file, e))
-    sys.exit(3)
+    if not loglevel in valid_loglevel:
+        print('Invalid loglevel %s supplied - Exiting' % loglevel)
+        sys.exit(1)
 
-# Build schema for MariaDb
-schema = {}
-for db in ('o3_qaddb', 'o3_sysdb'):
-    # Connect to MariaDB Platform
-    conn = con_mariadb(cfg, db)
+    '''
+        Simple logging setup
+    
+    formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(
+        level=logging.INFO, format=formatter, datefmt="[%X]", handlers=[logging.StreamHandler()]
+    )
+    '''
 
-    # Built schema
-    schema[db] = mdb_gettables(conn)
+    ''' 
+        More complex setup for logging
+    '''
 
-    for table in schema[db].keys():
-        schema[db][table] = mdb_getfields(conn,table)
+    # Setup logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(loglevel)
 
-    print('Number of tables (MariaDB %s): %d' % (db,len(schema[db].keys())))
-    conn.close()
+    # Logfile log output
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)  # Everything will be logged - based on logger.setLevel
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
 
-'''
-print(json.dumps(schema, indent=3))
-for t in schema[cfg['database']].keys():
-    print('\nFields for table %s:' % t)
-    print(len('Fields for table %s:' % t)*'-')
-    for f in schema[cfg['database']][t].keys():
-        print('Field: %s' % f)
-        print(len('Field: %s' % f)*'-')
-        for k in schema[cfg['database']][t][f].keys():
-            print('%s : %s' % (k,schema[cfg['database']][t][f][k]))
-        print()
+    # Console log output
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)     # by default INFO or higher will be displayed
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='[%X]')
+    ch.setFormatter(formatter)
 
-'''
+    logger.addHandler(fh)
+    logger.addHandler(ch)
 
-# Verify tables from OpenEdge with MariaDB
-#dir = ('../stage_qadeam', '../stage_qadadm', '../stage_qaddb')
-dir = ('../oeschema',)
-
-schema_suffix = 'dat'
-index_suffix = 'idx'
-data_suffix = 'dump'
-
-# Get schema for OpenEdge (oe)
-oetables = []
-oeschema = {}
-for d in dir:
-    oefiles = glob.glob(d + '/*.%s' % schema_suffix)
-    for oefile in oefiles:
-        fields = oem_getfields(oefile)
-        oefile = oefile.split('/')[-1]
-        oetables.append(oefile.split('.')[0])
-        oeschema[oefile.split('.')[0]] = fields
-print('Number of tables (OpenEdge databases): %d' % len(oeschema.keys()))
-
-notfound = []
-for oetable in oetables:
-    found = False
+    if not os.path.isfile(config_file):
+        logger.error('File %s could not be found - Exiting' % config_file)
+        sys.exit(1)
+    
+    try:
+        with open(config_file, 'r') as file:
+            cfg = yaml.safe_load(file)
+    except PermissionError:
+        logger.error('Not enough permissions to open file %s' % config_file)
+        sys.exit(2)
+    except Exception as e:
+        logger.error('Failed to open file %s - %s' % (config_file, e))
+        sys.exit(3)
+    
+    # Build schema for MariaDb
+    schema = {}
     for db in ('o3_qaddb', 'o3_sysdb'):
-        if oetable in schema[db]:
-            found = True
-    if found == False:
-        notfound.append(oetable)
-
-changename = []
-for oetable in notfound:
-    t = oetable + '_'
-    for db in ('o3_qaddb', 'o3_sysdb'):
-        if t in schema[db].keys():
-            changename.append(oetable)
-            break
-
-# Remove tables with changed names from notfound[]
-for t in changename:
-    if t in notfound:
-        notfound.remove(t)
-
-print('Tables with changed names - %s' % changename)
-print('Not found in MariaDB: %s' % notfound)
-
-print()
-# Verify OpenEdge tables with tables in MariaDB
-compare = {}
-for oetable in oeschema.keys():
-    mdbtable = oetable
-    if oetable in notfound:
-        continue
-    if oetable in schema['o3_qaddb']:
-        mdbname = 'o3_qaddb'
-    elif oetable in schema['o3_sysdb']:
-        mdbname = 'o3_sysdb'
-
-    compare[oetable] = compare_table(mdbname, oetable, changename)
-
-    if oetable in changename:
-        mdbtable = oetable + '_'
-
-    for field in compare[oetable].keys():
-        # Store data in variables
-        oetype = compare[oetable][field]['type'][0]
-        mdbtype = compare[oetable][field]['type'][1]
-        oefmt = compare[oetable][field]['format'][0]
-        mdbfmt = compare[oetable][field]['format'][1]
-
-        # Check existence in MariaDB
-        if not (int(oeschema[oetable][field]['extend']) > 0 or oeschema[oetable][field]['type'] == 'datetime-tz'):
-            # Field is not an extent or is of datatype datetime-tz
-            if not field in schema[mdbname][mdbtable].keys():
-                print('Field %s not found in %s.%s' % (field, mdbname, mdbtable))
-
-        # Check for valid datatype conversion
-        if not compare[oetable][field]['type'] in map_dt_oem_mdb:
-            print('Field %s of %s.%s datatype issue - %s' % (field, mdbname, oetable, compare[oetable][field]['type']))
-
-        # Check format
-        if not oefmt == mdbfmt and not mdbtype in dt_no_fmt:
-            # Differences in format
-            if oetype == 'decimal':
-                if int(oefmt) > int(mdbfmt.split(',')[0]):
-                    print('%s.%s.%s format issue - %s' % (mdbname, oetable, field, compare[oetable][field]['format']))
-            elif int(oefmt) > int(mdbfmt):
-                print('%s.%s.%s format issue - %s' % (mdbname, oetable, field, compare[oetable][field]['format']))
-
-
-#print(json.dumps(compare['rpttmp_mstr'], indent=3))
+        # Connect to MariaDB Platform
+        conn = con_mariadb(cfg, db)
+    
+        # Built schema
+        schema[db] = mdb_gettables(conn)
+    
+        for table in schema[db].keys():
+            schema[db][table] = mdb_getfields(conn,table)
+    
+        logger.info('Number of tables (MariaDB %s): %d' % (db,len(schema[db].keys())))
+        conn.close()
+    
+    '''
+    print(json.dumps(schema, indent=3))
+    for t in schema[cfg['database']].keys():
+        print('\nFields for table %s:' % t)
+        print(len('Fields for table %s:' % t)*'-')
+        for f in schema[cfg['database']][t].keys():
+            print('Field: %s' % f)
+            print(len('Field: %s' % f)*'-')
+            for k in schema[cfg['database']][t][f].keys():
+                print('%s : %s' % (k,schema[cfg['database']][t][f][k]))
+            print()
+    
+    '''
+    
+    # Verify tables from OpenEdge with MariaDB
+    #dir = ('../stage_qadeam', '../stage_qadadm', '../stage_qaddb')
+    dir = ('../oeschema',)
+    
+    schema_suffix = 'dat'
+    index_suffix = 'idx'
+    data_suffix = 'dump'
+    
+    # Get schema for OpenEdge (oe)
+    oetables = []
+    oeschema = {}
+    for d in dir:
+        oefiles = glob.glob(d + '/*.%s' % schema_suffix)
+        for oefile in oefiles:
+            fields = oem_getfields(oefile)
+            oefile = oefile.split('/')[-1]
+            oetables.append(oefile.split('.')[0])
+            oeschema[oefile.split('.')[0]] = fields
+    logger.info('Number of tables (OpenEdge databases): %d' % len(oeschema.keys()))
+    
+    notfound = []
+    for oetable in oetables:
+        found = False
+        for db in ('o3_qaddb', 'o3_sysdb'):
+            if oetable in schema[db]:
+                found = True
+        if found == False:
+            notfound.append(oetable)
+    
+    changename = []
+    for oetable in notfound:
+        t = oetable + '_'
+        for db in ('o3_qaddb', 'o3_sysdb'):
+            if t in schema[db].keys():
+                changename.append(oetable)
+                break
+    
+    # Remove tables with changed names from notfound[]
+    for t in changename:
+        if t in notfound:
+            notfound.remove(t)
+    
+    logger.info('Tables with changed names - %s' % changename)
+    logger.info('Not found in MariaDB: %s' % notfound)
+    
+    # Verify OpenEdge tables with tables in MariaDB
+    compare = {}
+    for oetable in oeschema.keys():
+        mdbtable = oetable
+        if oetable in notfound:
+            continue
+        if oetable in schema['o3_qaddb']:
+            mdbname = 'o3_qaddb'
+        elif oetable in schema['o3_sysdb']:
+            mdbname = 'o3_sysdb'
+    
+        compare[oetable] = compare_table(mdbname, oetable, changename)
+    
+        if oetable in changename:
+            mdbtable = oetable + '_'
+    
+        for field in compare[oetable].keys():
+            # Store data in variables
+            oetype = compare[oetable][field]['type'][0]
+            mdbtype = compare[oetable][field]['type'][1]
+            oefmt = compare[oetable][field]['format'][0]
+            mdbfmt = compare[oetable][field]['format'][1]
+    
+            # Check existence in MariaDB
+            if not (int(oeschema[oetable][field]['extend']) > 0 or oeschema[oetable][field]['type'] == 'datetime-tz'):
+                # Field is not an extent or is of datatype datetime-tz
+                if not field in schema[mdbname][mdbtable].keys():
+                    logger.warning('Field %s not found in %s.%s' % (field, mdbname, mdbtable))
+    
+            # Check for valid datatype conversion
+            if not compare[oetable][field]['type'] in map_dt_oem_mdb:
+                logger.warning('Field %s of %s.%s datatype issue - %s' % (field, mdbname, oetable, compare[oetable][field]['type']))
+    
+            # Check format
+            if not oefmt == mdbfmt and not mdbtype in dt_no_fmt:
+                # Differences in format
+                if oetype == 'decimal':
+                    if int(oefmt) > int(mdbfmt.split(',')[0]):
+                        logger.warning('%s.%s.%s format issue - %s' % (mdbname, oetable, field, compare[oetable][field]['format']))
+                elif int(oefmt) > int(mdbfmt):
+                    logger.warning('%s.%s.%s format issue - %s' % (mdbname, oetable, field, compare[oetable][field]['format']))
+    
+    
+    #print(json.dumps(compare['rpttmp_mstr'], indent=3))
