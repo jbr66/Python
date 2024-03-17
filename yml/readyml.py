93a4f914a852169b41d5176a395dc873a36dca42
jbr66 <brink66@gmail.com>
2024-03-17 14:31:49 +0100

    Verified code with pylama

--- yml/readyml.py
+++ yml/readyml.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 '''
 NAME
-	readyml.py - Read yaml file
+    readyml.py - Read yaml file
 
 AUTHOR
-	Written by John Brink
+    Written by John Brink
 
 REVISION HISTORY
 -------------------------------------------------------
-	1.0	12/01/2023 john - Initial version
+    1.0    12/01/2023 john - Initial version
         1.1     12/09/2023 john - Using input parameters
-	1.2	02/08/2024 john - Example for config
+    1.2    02/08/2024 john - Example for config
 -------------------------------------------------------
 vim:ts=4
 '''
 
-import os, sys
+import os
+import sys
 import yaml
 import json
 import argparse
@@ -23,11 +24,9 @@ import argparse
 # Define parameters
 
 parser = argparse.ArgumentParser(
-        description="Process given yaml file"
-        )
+    description="Process given yaml file")
 parser.add_argument('file',
-        help="provide yaml based file"
-        )
+                    help="provide yaml based file")
 args = parser.parse_args()
 
 file2read = args.file
@@ -42,18 +41,18 @@ try:
 except PermissionError:
     print("Not enough permissions to open file {}".format(file2read))
     sys.exit(2)
-except e:
-    print("Failed to open file {}".format(file2read))
+except Exception as e:
+    print("Failed to open file {} - {}".format(file2read, e))
     sys.exit(3)
 
 print(ex)
-print(42*'-')
+print(42 * '-')
 print("Output as JSON")
 print(json.dumps(ex, indent=2))
-print(42*'-')
+print(42 * '-')
 
 print('Example of config:')
 for i in ex:
-	print('{} : {}'.format(i,ex[i]))
+    print('{} : {}'.format(i, ex[i]))
 
 print('Mapping: {}'.format(ex['mappings']['character']))
